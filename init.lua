6f5be50ee070bfee16c10a56efc145c6d89f95fd
keith <keithroe@gmail.com>
2025-08-25 15:29:49 -0600

    fix bad merge in colorscheme setup code

--- init.lua
+++ init.lua
@@ -999,27 +999,46 @@ require('lazy').setup({
   --      vim.cmd.hi 'Comment gui=none'
   --    end,
   -- },
+  
+--  { -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --    'folke/tokyonight.nvim',
+  --    priority = 1000, -- Make sure to load this before all the other start plugins.
+  --    init = function()
+  --      -- Load the colorscheme here.
+  --      -- Like many other themes, this one has different styles, and you could load
+  --      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --      vim.cmd.colorscheme 'tokyonight-storm'
+  --      --vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --      -- You can configure highlights by doing something like:
+  --      vim.cmd.hi 'Comment gui=none'
+  --    end,
+  -- },
 
-  {
-    'rmehri01/onenord.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+ -- {
+ --   'rmehri01/onenord.nvim',
+ --   priority = 1000, -- Make sure to load this before all the other start plugins.
+ --   --config = function()
+ --   ---@diagnostic disable-next-line: missing-fields
+ --   --  require('onenord').setup {
+ --   --    styles = {
+ --   --      comments = { italic = false }, -- Disable italics in comments
+ --   --    },
+ --   --  }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'onenord'
+ --     -- Load the colorscheme here.
+ --     -- Like many other themes, this one has different styles, and you could load
+ --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+ --     vim.cmd.colorscheme 'onenord'
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+ --     -- You can configure highlights by doing something like:
+ --     vim.cmd.hi 'Comment gui=none'
+ --   end,
+ -- },
 
   {
     'gbprod/nord.nvim',
